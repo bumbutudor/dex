02b9d85cfe70c4664e2b3b2e27147ae385f89fa7
bumbutudor <bumbutudor10@gmail.com>
2021-10-05 15:14:42 +0300

    Insert c-e letters in dictionarul explicativ

--- routes/web.php
+++ routes/web.php
@@ -72,10 +72,11 @@ Route::put('contacts/{contact}/restore')->name('contacts.restore')->uses('Contac
 
 // Insert synonyms into DB
 Route::get('/insert-dictionary', function(){
-	$storage_path_synonyms = 'sinonime/synonyms_to_S_no_L.json';
-	$storage_path_explicativ = 'explicativ/explicativ_ac.json';
+	// $storage_path_synonyms = 'sinonime/synonyms_to_S_no_L.json';
+	// $storage_path_explicativ = 'explicativ/explicativ_ac.json';
+	$storage_path_explicativ_ce = 'explicativ/explicativ_ce.json';
 
-	$json = file_get_contents(storage_path($storage_path_explicativ));
+	$json = file_get_contents(storage_path($storage_path_explicativ_ce));
 	$objs = json_decode($json,true);
 	foreach ($objs as $obj)  {
 		foreach ($obj as $key => $value) {
