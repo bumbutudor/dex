f16e6c610ff082eb94e3f3434d3e36e8c99251d4
bumbutudor <bumbutudor10@gmail.com>
2022-03-15 15:42:26 +0200

    scroll to top after edit

--- resources/js/Shared/Layout.js
+++ resources/js/Shared/Layout.js
@@ -18,7 +18,7 @@ export default function Layout({ title, children }) {
           <div className="flex flex-grow overflow-hidden">
             <MainMenu className="flex-shrink-0 hidden w-56 py-12 px-4 overflow-y-auto bg-indigo-800 md:block" />
             {/* To reset scroll region (https://inertiajs.com/pages#scroll-regions) add `scroll-region="true"` to div below */}
-            <div className="w-full px-4 py-8 overflow-hidden overflow-y-auto md:p-12">
+            <div scroll-region="true" className="w-full px-4 py-8 overflow-hidden overflow-y-auto md:p-12">
               <FlashMessages />
               {children}
             </div>
